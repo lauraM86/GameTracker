2f413d2c6013d139630eaf4da289102224d72b43
john <johnsebastianveggonzalez@gmail.com>
2025-11-15 21:40:50 -0500

    Routes: correcciones en rutas de juegos y reviews

--- gametracker-backend/src/routes/gameRoutes.js
+++ gametracker-backend/src/routes/gameRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { getGames, createGame } from "../controllers/gameController.js";
+import {
+  getGames,
+  getGameById,
+  createGame,
+  updateGame,
+  deleteGame,
+} from "../controllers/gameController.js";
 
 const router = express.Router();
 
 router.get("/", getGames);
+router.get("/:id", getGameById);
 router.post("/", createGame);
+router.put("/:id", updateGame);
+router.delete("/:id", deleteGame);
 
 export default router;
